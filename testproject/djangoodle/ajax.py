7d07efdca4e28d7ca1abcdc45fe7dcd04452dd1d
Matija Piškorec <matija.piskorec@gmail.com>
2013-10-29 22:02:46 +0100

    Dajax form submission not working

--- testproject/djangoodle/ajax.py
+++ testproject/djangoodle/ajax.py
@@ -1,6 +1,26 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
+from django.shortcuts import get_object_or_404, render
+from djangoodle.models import Event, Participant
+from djangoodle.forms import EventForm, ParticipantForm
 
 @dajaxice_register
 def sayhello(request):
+    return simplejson.dumps({'message':'Hello World'})
+
+@dajaxice_register
+def add_participant(request,event_id):
+    e = get_object_or_404(Event, pk=event_id)
+    if request.method == 'POST':
+        form = ParticipantForm(request.POST)
+        print form
+        # if form.is_valid():
+        name = form.cleaned_data['name']
+        # print "Event_id = " + str(event_id)
+        p = Participant(name=name,event=e)
+        p.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # return HttpResponseRedirect(reverse('djangoodle:event_detail', args=(p.id,)))
     return simplejson.dumps({'message':'Hello World'})
--- testproject/djangoodle/urls.py
+++ testproject/djangoodle/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, patterns, include
 from djangoodle import views
+# from djangoodle import ajax
 
 urlpatterns = patterns('',
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^(?P<pk>\d+)/$',views.EventDetailView.as_view(), name='event'),	
 	url(r'^event/$',views.event_new, name='event_new'),	
 	url(r'^event/(?P<event_id>\d+)/$', views.event_detail, name='event_detail'),
+	# url(r'^event/(?P<event_id>\d+)/add_participant/$', ajax.add_participant, name='add_participant'),
 	)
